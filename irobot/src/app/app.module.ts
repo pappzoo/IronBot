4811742db11cf2da90c4480213bf62e2877b216d
Papp Zoltán <pappzoo@gmail.com>
2023-11-07 09:46:22 +0100

    unnecessary files removed

--- irobot/src/app/app.module.ts
+++ irobot/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -18,7 +17,6 @@ import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
     RobotlistaComponent
   ],
   imports: [
